0f0a47e062e799cd9024229f0d0f5d055f809227
jakobowsky <jakob.wolitzki@gmail.com>
2019-05-03 21:21:25 +0200

    entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Topic(models.Model):
     """ A topic the user is learning about """
     text = models.CharField(max_length=200)
@@ -8,3 +9,18 @@ class Topic(models.Model):
     def __str__(self):
         """ Return a string representation of model. """
         return self.text
+
+
+class Entry(models.Model):
+    """ Something specific learned about a topic  """
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            """Return a string representation of the model."""
+            return self.text[:50] + "..."
