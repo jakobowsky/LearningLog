f4d3c73ed38e523f8c9da04b9a5f7dff8090e4cd
jakobowsky <jakob.wolitzki@gmail.com>
2019-05-05 16:26:59 +0200

    new template

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, topics, topic
+
+app_name = 'learning_logs'
 
 urlpatterns = [
-    path('', index, name='index') 
+    path('', index, name='index'),
+    path('topics/', topics, name='topics'),
+    path('topics/<int:topic_id>/', topic, name='topic'),
 ]
