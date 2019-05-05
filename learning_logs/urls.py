a84cff99ab9e4a2fb2c3a0e3300a3727e1fcc408
jakobowsky <jakob.wolitzki@gmail.com>
2019-05-05 17:38:11 +0200

    edit entry done

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, topics, topic, new_topic, new_entry
+from .views import index, topics, topic, new_topic, new_entry, edit_entry
 
 app_name = 'learning_logs'
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('topics/<int:topic_id>/', topic, name='topic'),
     path('new_topic/', new_topic, name='new_topic'),
     path('new_entry/<int:topic_id>/', new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>/', edit_entry, name='edit_entry'),
+
 ]
