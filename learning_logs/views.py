7b5df1c8adb0b26a5e008925735578dae6e31f27
jakobowsky <jakob.wolitzki@gmail.com>
2019-05-07 20:27:09 +0200

    login required

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,7 +11,7 @@ def index(request):
     """ The home page for Learning Log """
     return render(request, 'learning_logs/index.html')
 
-
+@login_required
 def topics(request):
     """ Show all the topics """
     topics = Topic.objects.order_by('date_added')
